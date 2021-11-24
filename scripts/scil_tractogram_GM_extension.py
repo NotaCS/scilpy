29536700a01ad6459a12cf66018b55df0fe7a0e0
Victor Nozais <victor.nozais@gmail.com>
2021-11-24 20:59:11 +0100

    Changed the algo and added the option to use an atlas

--- scripts/scil_tractogram_GM_extension.py
+++ scripts/scil_tractogram_GM_extension.py
@@ -8,6 +8,10 @@ to ensure that the streamline end in the grey matter:
     - if the lengthening gets out of the grey matter, the protruding points will be cut.
     - if despite the lengthening, the endpoint does not reach the grey matter, the
     lengthening will be canceled for that streamline end.
+
+The "with_atlas" option consider that a point is "protruding" (and thus must be shaved)
+if the extension changes parcel (or get out of the grey matter). This help avoid the
+extension crossing hemishere, for example.
 """
 
 import nibabel as nib
@@ -32,7 +36,7 @@ def _build_arg_parser():
     p.add_argument('in_tractogram',
                    help='Path of the input tractograms (.trk).')
     p.add_argument('in_grey_matter',
-                   help='Path of the brain mask of the grey matter (.nii, nii.gz).'
+                   help='Path of the brain mask or atlas of the grey matter (.nii, nii.gz).'
                         '\n(used as anatomy reference for the tractogram)')
     p.add_argument('out_file',
                    help='Output file path (.trk).')
@@ -44,6 +48,10 @@ def _build_arg_parser():
                    default=0.5,
                    help='Step size for the added bit (in mm). Should be a multiple'
                         '\nof the length to add (Default=0.5mm).')
+    p.add_argument('-a', '--with_atlas', action='store_true',
+                   help='If chosen, the grey matter volume must be an atlas, and the '
+                        'shaving step of the extension will take into account if the '
+                        'extension changes parcel (and will cut it).')
     add_overwrite_arg(p)
 
     return p
@@ -69,7 +77,7 @@ def compute_end_bits(strml, step_nb, step_sz, nback=1):
     -------
     end_bits : Array
         4D array of shape (strml_nb, 2, step_nb, 3) contaning the added points for the
-        two end-points of each streamline
+        two end-points of each streamline. Each extension are ordered from streamline -> outward
 
     """
     end_bits = np.zeros((len(strml), 2, step_nb, 3), dtype='f')
@@ -89,52 +97,63 @@ def compute_end_bits(strml, step_nb, step_sz, nback=1):
     return end_bits
 
 
-def generate_longer_streamlines(strml, end_bits_vox, gm_vox):
+def generate_longer_streamlines(strml, end_bits_vox, atlas):
     """
     Generator that uses end_bits (in voxel space) and lengthen the streamlines while checking if the added length
     reach the GM or get out of the GM. If it doesn't reach the GM, the streamline is not lengthened,
     and if the added end get out of the GM, the part going out is shaved.
     """
-    # for i, strm in enumerate(strml):
-    #     end1 = end_bits_vox[i, 0, :]
-    #     while len(end1) and not tuple(end1[-1].astype(int)) in gm_vox:
-    #         end1 = np.delete(end1, -1, axis=0)
-    #     end2 = end_bits_vox[i, 1, :]
-    #     while len(end2) and not tuple(end2[-1].astype(int)) in gm_vox:
-    #         end2 = np.delete(end2, -1, axis=0)
-    #     newStrm = np.concatenate((end1[::-1], strm, end2))
-    #     yield newStrm
+    parcels = np.unique(atlas).tolist()
+    try:
+        parcels.remove(0)
+    except ValueError:
+        pass
     for i, strm in enumerate(strml):
+
         end1 = end_bits_vox[i, 0, :]
-        nv = 0  # Voxels not keep in the extension
-        testGM = False
+        nv = 0  # Voxels to keep in the extension
+        valParc = 0
         for v in end1:
+            if np.any(v < 0):
+                break  # Voxel out of the bounding box
             v = tuple(v.astype(int))
-            testGM = (v in gm_vox) or testGM  # Has it reached GM yet?
-            if testGM and v not in gm_vox:  # If it gets out of the GM
-                break
+            valv = atlas[v]
+            if not valParc and valv not in parcels:
+                pass  # GM not reached yet
+            elif not valParc and valv in parcels:
+                valParc = valv  # Just reached the GM
+            elif valParc and valv == valParc:
+                pass  # in the GM
+            elif valParc and valv != valParc:
+                break  # Changed parcel or got out of GM
             nv += 1
-        if testGM:
+        if valParc:
             end1 = end1[:nv]
         else:  # If it never reached GM
             end1 = end1[:0]
 
         end2 = end_bits_vox[i, 1, :]
-        nv = 0  # Voxels not keep in the extension
-        testGM = False
+        nv = 0  # Voxels to keep in the extension
+        valParc = 0
         for v in end2:
             v = tuple(v.astype(int))
-            testGM = (v in gm_vox) or testGM  # Has it reached GM yet?
-            if testGM and v not in gm_vox:  # If it gets out of the GM
-                break
+            valv = atlas[v]
+            if not valParc and valv not in parcels:
+                pass  # GM not reached yet
+            elif not valParc and valv in parcels:
+                valParc = valv  # Just reached the GM
+            elif valParc and valv == valParc:
+                pass  # in the GM
+            elif valParc and valv != valParc:
+                break  # Changed parcel or got out of GM
             nv += 1
-        if testGM:
+        if valParc:
             end2 = end2[:nv]
         else:  # If it never reached GM
             end2 = end2[:0]
+
         newStrm = np.concatenate((end1[::-1], strm, end2))
         yield newStrm
-
 
 # %% def main():
 t0 = time.time()
@@ -155,6 +174,8 @@ step_number = int(added_len//step_size)
 
 gm_im = nib.load(gm_F)
 gm = gm_im.get_fdata(dtype='f')
+if not args.with_atlas:
+    gm = np.where(gm, 1, 0)
 
 print('Loading the tractogram...')
 trk_sft = load_tractogram(trk_F, 'same', bbox_valid_check=False)
@@ -167,11 +188,23 @@ endBits = compute_end_bits(trk_sft.streamlines, step_number, step_size)
 endBits = apply_affine(np.linalg.inv(trk_sft.affine), endBits)  # To voxel space
 trk_sft.to_vox()
 
-voxel_GM = np.argwhere(gm)
-voxel_GM = set([tuple(ind) for ind in voxel_GM])
+# voxel_GM = {}
+# if args.with_atlas:
+#     parcels = np.unique(gm).tolist()
+#     try:
+#         parcels.remove(0)
+#     except ValueError:
+#         pass
+#     for parc in parcels:
+#         voxelGM = np.argwhere(gm==parc)
+#         voxel_GM[parc] = set([tuple(ind) for ind in voxelGM])
+
+# voxelGM = np.argwhere(gm)
+# voxel_GM['gm'] = set([tuple(ind) for ind in voxelGM])
 
 print('Shaving bad points...')
-strm_gen = generate_longer_streamlines(trk_sft.streamlines, endBits, voxel_GM)
+
+strm_gen = generate_longer_streamlines(trk_sft.streamlines, endBits, gm)
 new_trk_sft = StatefulTractogram(ArraySequence(strm_gen), trk_F, trk_sft.space)
 
 save_trk(new_trk_sft, args.out_file, bbox_valid_check=False)
